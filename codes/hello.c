51c1c1e97c17b095f4ecf9620e957203616e61e0
Saurav008 <33387479+Saurav008@users.noreply.github.com>
2018-01-19 10:08:47 +0530

    Create hello.c

---
+++ codes/hello.c
@@ -0,0 +1,6 @@
+// This is program to print HELLO WORLD in the output Screen.
+#include <Stdio.h> // This is header file which is to be included from library of C , which is standard input output funtion.
+void main()
+{
+    printf("HELLO WORLD"); // this is keyword for printing any value , statement etc.
+}
